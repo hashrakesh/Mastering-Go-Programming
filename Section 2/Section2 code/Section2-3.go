f9df6b5d5030ec296731b2b836974bb7ff9e503c
hashrakesh <hashrakeshkumar@gmail.com>
2017-07-17 11:56:14 +0530

    comments added

--- Section 2/Section2 code/Section2-3.go
+++ Section 2/Section2 code/Section2-3.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Counting")
 	for i := 1; i <= 4; i++ {
-		defer fmt.Println(i)
+		fmt.Println(i) // print normaly i.e. FIFO
+		defer fmt.Println(i)  // output in reverse i.e. LIFO
 	}
 	fmt.Prinln("Done")
 }
